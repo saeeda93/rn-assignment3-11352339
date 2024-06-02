1ce28125c8f88da3dc026011c5857b71c364d8d7
saeeda <saeedatofa@gmail.com>
2024-06-02 22:59:37 +0000

    initiate

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Image, TextInput, StyleSheet, ScrollView, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Free Palestine</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F7F0E8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  
-  text: {
-    fontSize: 24, 
-  },
-});
+const categories = [
+  { name: 'Exercise', tasks: '12 Tasks', imageUri: 'https://cdn.builder.io/api/v1/image/assets/TEMP/07bf8886dbde741814b101101343ebc9ee6120beeaa8d30074df3b012b2db71b?apiKey=84db356c45aa4a6ba9aaac34a35b9fe0&' },
+  { name: 'Study', tasks: '12 Tasks', imageUri: 'https://cdn.builder.io/api/v1/image/assets/TEMP/98c375d68ef9e063a7f499d7d1e7945212be5f7822ba7ae715f3d9a986895f91?apiKey=84db356c45aa4a6ba9aaac34a35b9fe0&' },
+  { name: 'Code', tasks: '15 Tasks', imageUri: 'https://cdn.builder.io/api/v1/image/assets/TEMP/07bf8886dbde741814b101101343ebc9ee6120beeaa8d30074df3b012b2db71b?apiKey=84db356c45aa4a6ba9aaac34a35b9fe0&' },
+  { name: 'Cook', tasks: '10 Tasks', imageUri: 'https://cdn.builder.io/api/v1/image/assets/TEMP/98c375d68ef9e063a7f499d7d1e7945212be5f7822ba7ae715f3d9a986895f91?apiKey=84db356c45aa4a6ba9aaac34a35b9fe0&' },
+  { name: 'Read', tasks: '5 Tasks', imageUri: 'https://cdn.builder.io/api/v1/image/assets/TEMP/07bf8886dbde741814b101101343ebc9ee6120beeaa8d30074df3b012b2db71b?apiKey=84db356c45aa4a6ba9aaac34a35b9fe0&' },
+  { name: 'Meditate', tasks: '7 Tasks', imageUri: 'https://cdn.builder.io/api/v1/image/assets/TEMP/98c375d68ef9e063a7f499d7d1e7945212be5f7822ba7ae715f3d9a986895f91?apiKey=84db356c45aa4a6ba9aaac34a35b9fe0&' },
+  { name: 'Travel', tasks: '2 Tasks', imageUri: 'https://cdn.builder.io/api/v1/image/assets/TEMP/07bf8886dbde741814b101101343ebc9ee6120beeaa8d30074df3b012b2db71b?apiKey=84db356c45aa4a6ba9aaac34a35b9fe0&' },
+  { name: 'Shop', tasks: '6 Tasks', imageUri: 'https://cdn.builder.io/api/v1/image/assets/TEMP/98c375d68ef9e063a7f499d7d1e7945212be5f7822ba7ae715f3d9a986895f91?apiKey=84db356c45aa4a6ba9aaac34a35b9fe0&' },
+];
